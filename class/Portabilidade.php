d38c821566bd8a941d8a0371efc42378a2efc2c4
Douglas Akamine <53135866+douglasakamine@users.noreply.github.com>
2020-08-27 11:32:54 +0900

    Update Portabilidade.php

--- class/Portabilidade.php
+++ class/Portabilidade.php
@@ -5,7 +5,7 @@ class Portabilidade {
 	private $operadora;
 
 	public function consultaOperadora($numero){
-		/*    $url_portabilidade = "http://201.20.37.222/UnionCRM/sms/bdo.jsp?numero=$numero";
+		/*    $url_portabilidade = "http://url_here?numero=$numero";
 		return $this->operadora = file_get_contents($url_portabilidade); // Consulta a portabilidade através da URL acima
 		if($operadora == "0")
 		$operadora = "FIXO";
